f9f3ef0433f6fd8fdd21ff84285bc7f3cfc5875f
HP ProDesk <mehedipy@gmail.com>
2018-07-21 03:03:03 -0700

    initial commit three classes inheriting Human and implementing (overriding) work() method

---
+++ src/com/sdmagp/iiuc/example/Main.java
@@ -0,0 +1,22 @@
+package com.sdmagp.iiuc.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Rattre rattre = new Rattre();
+        Tazin tazin = new Tazin();
+        Maftuha maftuha = new Maftuha();
+
+
+        rattre.sleeps();
+        rattre.works();
+
+        tazin.sleeps();
+        tazin.works();
+
+        maftuha.sleeps();
+        maftuha.works();
+
+    }
+}
