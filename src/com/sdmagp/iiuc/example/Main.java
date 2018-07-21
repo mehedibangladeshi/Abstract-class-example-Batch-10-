929144976eabd4240ce030d602d158b52fca955f
HP ProDesk <mehedipy@gmail.com>
2018-07-21 03:23:22 -0700

    Second commit

--- src/com/sdmagp/iiuc/example/Main.java
+++ src/com/sdmagp/iiuc/example/Main.java
@@ -4,19 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Rattre rattre = new Rattre();
-        Tazin tazin = new Tazin();
-        Maftuha maftuha = new Maftuha();
+       Human rattre = new Rattre();
+       Human onu = new Human() {
+           @Override
+           void works() {
+         System.out.println(" Clean ");
+           }
+       };
+
+       rattre.sleeps();
+       rattre.works();
+
+       onu.sleeps();
+       onu.works();
 
 
-        rattre.sleeps();
-        rattre.works();
 
-        tazin.sleeps();
-        tazin.works();
 
-        maftuha.sleeps();
-        maftuha.works();
 
     }
 }
